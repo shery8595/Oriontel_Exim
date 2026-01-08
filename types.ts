8c3873557c57610e7a800f116c91d1d64fde44e2
shehryar rehman <131074050+shery8595@users.noreply.github.com>
2026-01-08 16:05:25 +0500

    Remove empty line in types.ts
    Remove unnecessary empty line at the start of types.ts

---
+++ types.ts
@@ -0,0 +1,16 @@
+export interface NavItem {
+  label: string;
+  href: string;
+}
+
+export interface Industry {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export interface Operation {
+  category: string;
+  icon: string;
+  items: string[];
+}
